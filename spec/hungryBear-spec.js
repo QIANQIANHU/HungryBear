df213fd79b86caac81dbebd8aeee9680d90c6b5c
Ainur Dzhaianbaeva and Qianqian Hu <ainur@>
2018-03-19 13:21:26 -0700

    add specs

--- spec/hungryBear-spec.js
+++ spec/hungryBear-spec.js
@@ -4,7 +4,8 @@ describe('hungryBear', function() {
    let fuzzy = new HungryBear("Fuzzy");
 
    beforeEach(function() {
-  jasmine.clock().install();
+     jasmine.clock().install();
+     fuzzy.setHunger();// utilize setHunger()
   });
 
   afterEach(function() {
@@ -13,6 +14,25 @@ describe('hungryBear', function() {
 
   it('should have a name and a food level of 10 when it is created', function() {
     expect(fuzzy.name).toEqual("Fuzzy");
+    expect(fuzzy.foodLevel).toEqual(10);
+  });
+  it('should have a food level of 6 after 4001 milliseconds', function() {
+    jasmine.clock().tick(4001);
+    expect(fuzzy.foodLevel).toEqual(6);//what is this?
+  });
+  it('should get very hungry if the food level drops below zero', function() {
+  fuzzy.foodLevel = 0;
+  expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should get very hungry if 10 seconds pass without feeding', function() {
+    jasmine.clock().tick(10001);
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should have a food level of ten if it is fed', function() {
+    jasmine.clock().tick(9001);
+    fuzzy.feed();
     expect(fuzzy.foodLevel).toEqual(10);
   });
 });
--- src/hungryBear.js
+++ src/hungryBear.js
@@ -11,4 +11,17 @@ export class HungryBear {
         this.foodLevel--;
       }, 1000);
     }
+
+  didYouGetEaten() {
+  if (this.foodLevel > 0) {
+    return false;
+    } else {
+    return true;
+    }
+  }
+
+  feed() {
+    this.foodLevel = 10;
+  }
 }
+//what's the relationship of class/constructor/methods/prototype?
